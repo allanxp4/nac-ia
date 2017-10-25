7dedc355c306069640453cec861bd3f0a1f1c795
logonrm <logonrm@fiap.com.br>
2017-10-25 19:26:16 -0200

    redo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import shuffle
 from sklearn import neighbors, datasets
+from sklearn.feature_extraction import DictVectorizer
 
 #atributos de teste
 attributes = [
@@ -22,9 +23,27 @@ for line in f:
     #faz split dos parametros
     split_line = line.split(',')
     #adiciona a um dicionario
-    items.append({'buying': split_line[0], 'maint': split_line[1], 'doors': split_line[2], 'persons': split_line[3], 'lug_boot': split_line[4], 'safety': split_line[5], 'original_classification': split_line[6]})
+    items.append({'buying': split_line[0], 'maint': split_line[1], 'doors': split_line[2],
+                  'persons': split_line[3], 'lug_boot': split_line[4], 'safety': split_line[5],
+                  'original_classification': split_line[6]})
 
 f.close()
+
+def getItems(rawItems):
+    items = rawItems.copy()
+    for item in items:
+        items.pop('original_classification')
+    return items
+
+def getClassifications(rawItems)
+    items = rawItems.copy()
+    for k in rawItems.keys():
+        if(k != 'original_classification'):
+            del items[k]
+    return items
+    
+
+
 
 #randomiza a lista
 shuffle(items)
@@ -39,7 +58,13 @@ print(items)
 print(training_set)
 print(classification_set)
 
-#clf = neighbors.KNeighborsClassifier(training_set)
+trainingXVectorizer = DictVectorizer()
+trainingYVectorizer = DictVectorizer()
+classificationYVectorizer = DictVectorizer()
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(trainingXVectorizer.fit_transform(getItems(training_set), trainingYVectorizer(getClassifications(training_set))
+print(clf.predict(classificationYVectorizer.fit_transform(classification_set)))
 
 
 
